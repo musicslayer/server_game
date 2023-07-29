46cddc5f333a9ce8f0221444c335271d0b7b7f05
musicslayer <musicslayer@gmail.com>
2023-07-29 14:29:58 -0400

    Update

--- js/entity/EntityFactory.js
+++ js/entity/EntityFactory.js
@@ -4,11 +4,11 @@ class EntityFactory {
     static id = 0;
     static entityMap = new Map();
 
-    static createInstance(className, number, ...args) {
+    static createInstance(className, stackSize, ...args) {
         let id = EntityFactory.id++;
 
         let entity = Reflection.createInstance(className, ...args) ?? Reflection.createInstance("UnknownEntity", ...args);
-        entity.stackSize = number;
+        entity.stackSize = stackSize;
         entity.id = id;
 
         EntityFactory.entityMap.set(id, entity);
