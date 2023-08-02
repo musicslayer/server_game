efe37ed7c6ed8d27a98071bc317496831ac92f44
musicslayer <musicslayer@gmail.com>
2023-08-02 14:27:37 -0400

    Update

--- js/AppState.js
+++ js/AppState.js
@@ -104,11 +104,9 @@ class AppState {
 
         // After loading data:
         // - Refresh all clients and players that are currently logged in.
-        // - Log out all of the clients.
-        // - Log out all of the players.
+        // - Log out all of the clients and players that are currently logged in.
         this.refresh();
-        this.logOutClients();
-        this.logOutPlayers();
+        this.logOut();
 
         // Start the new servers' ticks.
         this.serverManager.startServerTicks();
@@ -125,26 +123,24 @@ class AppState {
         }
     }
 
-    logOutClients() {
-        for(let key of this.clientManager.clientMap.keys()) {
-            let client = this.clientManager.getClient(key);
-            client.socket.disconnect(true);
-        }
-
-        this.clientManager.clientMap = new Map();
-    }
-
-    logOutPlayers() {
+    logOut() {
         for(let account of this.accountManager.accounts) {
             for(let key of account.characterMap.keys()) {
                 let player = account.getCharacter(key).player;
 
-                if(player.isSpawned) {
-                    let serverTask = new ServerTask((player) => {
-                        player.doDespawn();
-                    }, player);
-    
-                    player.getServer().scheduleTask(undefined, 0, 1, serverTask);
+                if(player.client) {
+                    // Disconnect the client, which will in turn despawn the player.
+                    player.client.socket.disconnect(true);
+                }
+                else {
+                    // This player has no client, so despawn the player manually.
+                    if(player.isSpawned) {
+                        let serverTask = new ServerTask((player) => {
+                            player.doDespawn();
+                        }, player);
+        
+                        player.getServer().scheduleTask(undefined, 0, 1, serverTask);
+                    }
                 }
             }
         }
