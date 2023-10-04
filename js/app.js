8af37794a0e8b0ae3738a412b58f93a757b94c97
musicslayer <musicslayer@gmail.com>
2023-10-04 00:44:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ const WorkerManager = require("./worker/WorkerManager.js");
 // --- Add in logging calls. This requires more storage space.
 // --- Use confirmation emails for any account changes. This requires a non-residential ISP that doesn't block port 25.
 
-// TODO Clicking both mouse buttons causes error.
-//  game:142 Uncaught (in promise) TypeError: undefined is not iterable (cannot read property Symbol(Symbol.iterator))
-//  at HTMLCanvasElement.<anonymous> (game:142:91)
-
 async function init() {
     let appState = new AppState();
     let appAdmin = new AppAdmin(appState);
